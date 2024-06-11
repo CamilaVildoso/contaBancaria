c431e48750ec08db671c264813dce0ce3012385d
Camila Vildoso <camiliun123@gmail.com>
2024-06-11 13:16:35 -0300

    Instanciar cores

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,13 @@
 package conta;
 
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 // Imprimir mensajes en diferentes colores
+		Cores.printRed("Este es un mensaje en rojo");
+		Cores.printBlue("Este es un mensaje en azul");
 	}
 
 }
--- src/conta/util/Cores.java
+++ src/conta/util/Cores.java
@@ -1,5 +1,19 @@
 package conta.util;
 
 public class Cores {
+	
+	// Métodos para obtener códigos de color 
+	public static final String RESET = "\033[0m";
+    public static final String RED = "\033[0;31m";
+    public static final String BLUE = "\033[0;34m";
 
+ // Método para imprimir un mensaje en rojo
+    public static void printRed(String message) {
+        System.out.println(RED + message + RESET);
+    }  
+    
+ // Método para imprimir un mensaje en azul
+    public static void printBlue(String message) {
+        System.out.println(BLUE + message + RESET);
+    }
 }
