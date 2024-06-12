0b893a3dd478345e91191e29a091e6807a4f5340
Camila Vildoso <camiliun123@gmail.com>
2024-06-12 15:17:10 -0300

    Classe Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,18 +1,27 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
+		//Teste classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Camila", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner leia = new Scanner(System.in);
 		
 		int opcao;
 		
 		while(true) {
-			System.out.println(Cores.TEXT_YELLOW+Cores.ANSI_BLACK_BACKGROUND+"***************************************************");
+			System.out.println(Cores.TEXT_YELLOW+Cores.ANSI_BLACK_BACKGROUND+"\n***************************************************");
 			System.out.println("                                                   ");
 			System.out.println("               BANCO DO BRAZIL COM Z               ");
 			System.out.println("                                                   ");
